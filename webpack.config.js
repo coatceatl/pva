cbb0902cc989ad334814149f269412c80861e3af
Oksana Tkachenko <oksana@crystax.net>
2018-08-23 01:18:28 +0300

    added font and added block benefits

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,16 @@ module.exports = {
           }
         }]
       },
+      {
+        test: /\.ttf$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
+      },
     ],
   },
   plugins: [
